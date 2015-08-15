b7a0b19fe2ab8b2a6d247ff4a10bfe0c92943424
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-08-15 19:57:57 +0200

    Get keys constants from selenium-webdriver. Use waitForVisible before click.

--- e2e/hello_spec.js
+++ e2e/hello_spec.js
@@ -1,27 +1,31 @@
 'use strict';
 
+var webdriver = require('selenium-webdriver');
+
 it('should load main page', function() {
   return browser
     .url('/')
+    .waitForVisible('#todoapp')
     .getTitle()
       .should.eventually.match(/Hello World/);
 });
 
 it('should add new todo', function() {
-  var ENTER_KEY = '\uE006';
-
   return browser
     .url('/')
+    .waitForVisible('#new-todo')
     .click('#new-todo')
-    .keys('blabla' + ENTER_KEY)
-    .isExisting('input.edit')
+    .keys('blabla' + webdriver.Key.ENTER)
+    .waitForVisible('input.edit')
       .should.eventually.be.ok;
 });
 
 it('should show active todos after click', function() {
   return browser
     .url('/')
+    .waitForVisible('a[ui-sref="active"]')
     .click('a[ui-sref="active"]')
+    .waitForVisible('a.selected[ui-sref="active"]')
     .url()
       .should.eventually.have.property('value')
         .that.have.string('#/active');
